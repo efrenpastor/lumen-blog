8f2252e5078cd7ee56a2f163ec188fd3f2f26e57
Efren Pastor <efrenpastor@gmail.com>
2021-05-25 16:52:16 +0200

    fix: usamos otro pattern para reconocer el bloque de amazon

--- src/cms/editor-components/amazon.js
+++ src/cms/editor-components/amazon.js
@@ -13,21 +13,18 @@ const Amazon = {
     { name: 'url', label: 'Url', widget: 'string' }
   ],
   // Pattern to identify a block as being an instance of this component
-  pattern: /^<amazon name="(.*)" image="(.*)" price="(.*)" url="(.*)">/,
+  pattern: /^{{< Amazon name="(.*)" image="(.*)" price="(.*)" url="(.*)" >}}$/,
   // Function to extract data elements from the regexp match
-  fromBlock: function (match) {
-    return {
+  fromBlock: match =>
+    match && {
       name: match[1],
       image: match[2],
       price: match[3],
       url: match[4]
-    };
-  },
+    },
   // Function to create a text block from an instance of this component
   toBlock: function(obj) {
-    return (
-      '<amazon name="' + obj.name + '" image="' + obj.image + '" price="' + obj.price + '" url="' + obj.url + '">'
-    );
+    return `{{< Amazon name=\"${obj.name}\" image=\"${obj.image}\" price=\"${obj.price}\" url=\"${obj.url}\" >}}`
   },
   // Preview output for this component. Can either be a string or a React component
   // (component gives better render performance)
